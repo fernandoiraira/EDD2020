a359cd47f145351a5483b1733c5261364275009b
Fernando Iraira <fmiraira@gmail.com>
2020-08-30 21:05:33 -0300

    Inicio de implementacion de ArbolBin

---
+++ src/Jerarquicas/ArbolBin.java
@@ -0,0 +1,41 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Jerarquicas;
+
+/**
+ *
+ * @author Sammy Guergachi <sguergachi at gmail.com>
+ */
+public class ArbolBin {
+    
+    NodoArbol raiz;
+    
+    public ArbolBin() {
+        this.raiz = null;
+    }
+    
+    public boolean insertar(Object elemNuevo, Object elemPadre, char lugar) {
+        boolean exito = true;
+        
+        if (this.raiz != null) {
+            this.raiz = new NodoArbol(elemNuevo, null, null);
+        } else {
+            
+        }
+        
+        return exito;
+    }
+    
+    private NodoArbol obtenerNodo(NodoArbol n, Object buscado) {
+        NodoArbol resultado = null;
+        
+        if (n != null) {
+            
+        }
+        
+    }
+    
+}
